54f529dd5d6ec7402b9c721b4aad7518d15dfe50
phamuyen120925 <phamuyen120925@gmail.com>
2026-01-21 13:16:45 +0700

    thuc hanh 2

--- FirstWebMVC/obj/Debug/net10.0/FirstWebMVC.AssemblyInfo.cs
+++ FirstWebMVC/obj/Debug/net10.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833cd5604bd267195342a939ac6e21cbc32b0391")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d794a3426351e1db055918743c14a8d2137f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
